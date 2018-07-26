40ff59b535cd1210f8026388408481f63f16475c
Rick Van <x.rckvan@gmail.com>
2018-07-26 12:07:16 -0600

    fixed timeinterval problems

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -1,7 +1,56 @@
 //start of break time!
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-    setInterval(() => {
-        seconds--;
-    }, 1000);
+    //clear any existing timers
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        //check if we should stop it!
+        if (secondsLeft < 0) {
+            clearInterval(countdown)
+            return;
+        }
+        //display it
+        displayTimeLeft(secondsLeft);
+
+    }, 1000)
 }
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const adjustedMinute = minutes < 10 ? '0' : "";
+    endTime.textContent = `Be Back At ${adjustedHour}:${adjustedMinute}${minutes}`
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+
+})
